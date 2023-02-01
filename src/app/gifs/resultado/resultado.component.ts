60db6d2903dc893124272418837eb91ab6568ca7
Jonathan <jonathan_corro@hotmail.com>
2023-02-01 01:10:52 -0500

    proyecto terminado

--- src/app/gifs/resultado/resultado.component.ts
+++ src/app/gifs/resultado/resultado.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-resultado',
@@ -6,6 +7,12 @@ import { Component } from '@angular/core';
 })
 export class ResultadoComponent {
 
+get resultados(){
+  return this.gifsService.resultados;
+}
 
+constructor (private gifsService: GifsService){
+
+}
 
 }
